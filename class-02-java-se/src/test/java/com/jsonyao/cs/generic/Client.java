c8c1480661701b9f8cd5b7e89c262de1d6604aac
yaocs2 <yaocs2@midea.com>
2020-11-17 16:18:11 +0800

    my-java-go-test> class-02-java-se 泛型测试代码更新

--- class-02-java-se/src/test/java/com/jsonyao/cs/generic/Client.java
+++ class-02-java-se/src/test/java/com/jsonyao/cs/generic/Client.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 /**
  * 泛型测试类
+ * A. Relation:
+ *      a. https://www.cnblogs.com/coprince/p/8603492.html
  */
 public class Client {
 
@@ -72,8 +74,8 @@ public class Client {
          *      B. 因此, 实现泛型接口时, 实现类实现的泛型接口:
          *          a. 实现类明确类型 => 实现类不用指定泛型
          *          b. 实现类不明确类型:
-         *              b.1. 使用Object类型 => 实现类和接口都不使用泛型
-         *              b.2. 不使用Object类型 => 实现类和接口保持相同的泛型型
+         *              b.1. 实现类和接口都不使用泛型 => 使用Object类型
+         *              b.2. 实现类和接口保持相同的泛型型 => 不使用Object类型
          */
 //        Generator myGenerator3 = new MyGenerator();// 编译通过, 实现类不指定泛型类型时, 引用也不需要使用泛型
 //        Generator<Integer> myGenerator2 = new MyGenerator();// 编译错误, 当实现类指定了泛型接口类型, 则泛型接口引用必须为该类型或者不使用泛型
@@ -82,95 +84,123 @@ public class Client {
 
         /**
          * 5、测试泛型方法
+         *      A. 结果:
+         *          a. 泛型类会有普通方法、使用了类泛型的普通方法、普通泛型方法、静态方法、静态泛型方法
+         *          b. 其中静态方法如果使用了泛型, 那么必然是泛型方法, 其泛型与类泛型不是同一个泛型
+         *      B. 因此, 使用类泛型的是普通方法, 声明了泛型的才是泛型方法, 泛型声明在修饰符与返回值之间的位置
          */
-
-
-
+//        Class testResultClazz5 = MyGeneric.test(new Integer(2)).getClass();// 编译通过, 返回Integer.class
+//        Class testResultClazz51 = MyGeneric.test("2").getClass();// 编译通过, 返回String.class
+//        MyGeneric myGeneric5 = new MyGeneric<>(new Integer(2));// 编译通过
+//        Class testResultClazz52 = myGeneric5.test(false).getClass();// 编译通过, 返回Boolean.class
 
         /**
-         * 4、类型通配符
+         * 6、测试泛型通配符
+         *      A. 结果:
+         *          a. 没有泛型时, 可以完成不同类型的参数传递
+         *          b. 形参为Object泛型时, 不同类型的参数传递失败
+         *          c. 使用泛型通配符时, 可以完成不同类型的参数传递
+         *      B. 因此, 使用泛型通配符可以解决参数类型传递的限制问题
          */
-        // 1、测试没有泛型是否可用
-        List<String> nameList = new ArrayList<>();
-        nameList.add("123");
-        test1(nameList);// 可用
+//        // 1、测试没有泛型是否可用
+//        List<String> nameList = new ArrayList<>();
+//        nameList.add("123");
+//        test1(nameList);// 编译通过, 没有泛型时可用
+//
+//        // 2、测试Object泛型是否可用
+////        test2(nameList);// 编译错误, 可见String属于Object类型, 但依然不能传递
+//
+//        // 3、测试通配符是否可用
+//        test3(nameList);// 编译通过, 统配符可用
 
-        // 2、测试Object泛型是否可用
-//        test2(nameList);// 不可用
-
-        // 3、测试通配符是否可用
-        test3(nameList);// 可用
-
-        // 4、测试泛型的上限 <? extends 类型>
-        Creature creature = new Creature();
-        List<Creature> creatureList = new ArrayList<>();
-
-        Animal animal = new Animal();
-        List<Animal> animalList = new ArrayList<>();
-
-        Dog dog = new Dog();
-        List<Dog> dogList = new ArrayList<>();
-
-        creatureList.add(creature);// 可用
-        creatureList.add(animal);// 可用
-        creatureList.add(dog);// 可用
-
-//        animalList.add(creature);// 不可用
-        animalList.add(animal);// 可用
-        animalList.add(dog);// 可用
-
-//        dogList.add(creature);// 不可用
-//        dogList.add(animal);// 不可用
-        dogList.add(dog);// 可用
-
-        testUp(dogList);// 可用
-        testUp(animalList);// 可用
-//        testUp(creatureList);// 不可用
+//        System.out.println();
 
         /**
-         * 4.1、测试泛型上限赋值 -> 代表容器至多为Animal类型
+         * 7、 测试泛型的上限 <? extends 类型>
+         *     A. 结果:
+         *          a. 泛型形参规定泛型上限时, 实参做多只能传递到上限类型, 即只能传递子类及其本类类型
+         *          b. 泛型类泛型规定泛型上限时, 同泛型方法
+         *          c. 泛型容器赋值规定泛型上限时, 禁止一切赋值, 只可以获取本类及其子类类型
          */
-        List<? extends Animal> upList = new ArrayList<>();
-//        upList.add(dog);// 不可用
-//        upList.add(animal);// 不可用
-//        upList.add(creature);// 不可用
-
-        upList = dogList;// 可用
-        upList = animalList;// 可用
-//        upList = creatureList;// 不可用
+//        // 7.1。 测试泛型参数传递
+//        Creature creature = new Creature();
+//        List<Creature> creatureList = new ArrayList<>();
+//
+//        Animal animal = new Animal();
+//        List<Animal> animalList = new ArrayList<>();
+//
+//        Dog dog = new Dog();
+//        List<Dog> dogList = new ArrayList<>();
+//
+//        testUp(dogList);// 编译通过
+//        testUp(animalList);// 编译通过
+////        testUp(creatureList);// 编译错误
+//
+//        // 7.2. 测试泛型上限赋值, 为了保证安全, 不能添加任何子类型
+//        List<? extends Animal> upList = new ArrayList<>();
+//
+////        upList.add(dog);// 编译错误, 直接使用父类类型List<Animal>即可, 不用<? extends Animal>这种结构
+////        upList.add(animal);// 编译错误, 直接使用父类类型List<Animal>即可, 不用<? extends Animal>这种结构
+////        upList.add(creature);// 编译错误, 直接使用父类类型List<Animal>即可, 不用<? extends Animal>这种结构
+//
+//        upList = dogList;// 编译通过
+//        upList = animalList;// 编译通过
+////        upList = creatureList;// 编译错误
+//
+        // 7.3. 测试泛型获取对象
+////        Dog dog1 = upList.get(0);// 编译错误, 由于通配符不知道存了什么子类, 所以不能下转型
+//        Animal animal1 = upList.get(0);// 编译通过, 但是可以上转型
+//        Creature creature1 = upList.get(0);// 编译通过, 但是可以上转型
 
         /**
-         * 4.2、测试泛型获取对象
+         * 8、测试泛型的上限 <? super 类型>
+         *     A. 结果:
+         *          a. 泛型形参规定泛型下限时, 实参做多只能传递下限类型以上类型, 即只能传递父类及其本类类型
+         *          b. 泛型类泛型规定泛型下限时, 同泛型方法
+         *          c. 泛型容器赋值规定泛型下限时, 只可以赋值本类及其子类类型, 禁止获取任何值
          */
-//        Dog dog1 = upList.get(0);// 不可用
-        Animal animal1 = upList.get(0);// 可用
-        Creature creature1 = upList.get(0);// 可用
+//        // 8.1. 测试泛型泛型参数传递
+//        Creature creature = new Creature();
+//        List<Creature> creatureList = new ArrayList<>();
+//
+//        Animal animal = new Animal();
+//        List<Animal> animalList = new ArrayList<>();
+//
+//        Dog dog = new Dog();
+//        List<Dog> dogList = new ArrayList<>();
+//
+//        testDown(creatureList);// 编译通过
+//        testDown(animalList);// 编译通过
+////        testDown(dog);// 编译错误
+//
+//        // 8.2. 测试泛型下限赋值
+//        List<? super Animal> downList = new ArrayList<>();
+//        downList.add(dog);// 编译通过
+//        downList.add(animal);// 编译通过
+////        downList.add(creature);// 编译错误
+//
+//        // 8.3. 测试泛型获取对象
+//        downList = dogList;// 编译错误
+//        downList = animalList;// 编译通过
+//        downList = creatureList;// 编译通过
+//
+////        Dog dog2 = downList.get(0);// 编译错误
+////        Animal animal2 = downList.get(0);// 编译错误
+////        Creature creature2 = downList.get(0);// 编译错误
 
         /**
-         * 5、测试泛型的上限 <? super 类型>
+         * 9、测试泛型数组
+         *      A. 结果:
+         *          a. 不可以创建一个确切泛型类型的数组
+         *          b. 但可以创建一个通配符泛型类型的数组, 且获取时必须进行类型转换
          */
-        testDown(creatureList);// 可用
-        testDown(animalList);// 可用
-//        testDown(dog);// 不可用
-
-        /**
-         * 5.1、测试泛型下限赋值 -> 代表容器装的是至少为Animal的类型
-         */
-        List<? super Animal> downList = new ArrayList<>();
-        downList.add(dog);// 可用
-        downList.add(animal);// 可用
-//        downList.add(creature);// 不可用
-
-//        downList = dogList;// 不可用
-        downList = animalList;// 可用
-        downList = creatureList;// 可用
-
-        /**
-         * 5.2、测试泛型获取对象 -> 由于
-         */
-//        Dog dog2 = downList.get(0);// 不可用
-//        Animal animal2 = downList.get(0);// 不可用
-//        Creature creature2 = downList.get(0);// 不可用
+//        List<String> [] testList =  new ArrayList<String>[10];// 编译错误, 不能创建一个确切泛型类型的数组
+        List<?>[] testList = new ArrayList<?>[10];// 编译通过, 但可以创建一个通配符泛型类型的数组
+        List<String> ls = new ArrayList<>();
+        ls.add("aaa");
+        testList[0] = ls;
+        String str = (String) testList[0].get(0);
+        System.out.println(str);
     }
 
     /**
@@ -195,7 +225,7 @@ public class Client {
      * 测试通配符是否可用
      * @param list
      */
-    public static void test3(List<?> list){// 可用
+    public static void test3(List<?> list){// 编译通过
         System.out.println(list.get(0));
         return;
     }
--- class-02-java-se/src/test/java/com/jsonyao/cs/generic/MyGeneric.java
+++ class-02-java-se/src/test/java/com/jsonyao/cs/generic/MyGeneric.java
@@ -7,14 +7,27 @@ public class MyGeneric<T> {
 
     private T genericCode;
 
+    // 静态方法-不含泛型
+    public static void main(String[] args) {
+
+    }
+
+    // 静态泛型方法-含泛型
+    public static <E> E test(E args) {
+        return args;
+    }
+
+    // 构造方法-使用类的泛型
     public MyGeneric(T genericCode) {
         this.genericCode = genericCode;
     }
 
+    // 普通方法-使用类的泛型
     public T getGenericCode() {
         return genericCode;
     }
 
+    // 普通方法-使用类的泛型
     public void setGenericCode(T genericCode) {
         this.genericCode = genericCode;
     }
