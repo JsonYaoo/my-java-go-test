183bddc0166c3b88c3317fc2d88a4f3801641797
JsonYaoo <1484073063@qq.com>
2020-12-30 23:18:34 +0800

    my-java-go-test> AtomicStampedReferenceDemo1更新

--- class-20-concurrent/src/main/java/com/jsonyao/cs/juc/atomic/aba/MyAtomicReferenceAbaDemo1.java
+++ class-20-concurrent/src/main/java/com/jsonyao/cs/juc/atomic/aba/MyAtomicReferenceAbaDemo1.java
@@ -1,26 +1,25 @@
 package com.jsonyao.cs.juc.atomic.aba;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicStampedReference;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * 测试AtomicStampedXXX解决ABA问题: 加入版本号, 下一个线程使用上一个版本号会更新失败
+ * 测试ABA问题: 同样的abc经过abcd ->abc, 对于第二个线程不应该能够设置成功, 但却设置成功了
  */
-public class AtomicStampedReferenceDemo1 {
+public class MyAtomicReferenceAbaDemo1 {
 
-    public static final AtomicStampedReference<String> ATOMIC_STAMPED_REFERENCE = new AtomicStampedReference<String>("abc", 0);
+    public static final AtomicReference<String> ATOMIC_REFERENCE = new AtomicReference<String>("abc");
 
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++){
             final int num = i;
-            final int stamp = ATOMIC_STAMPED_REFERENCE.getStamp();
             new Thread(() -> {
                 try {
                     TimeUnit.MICROSECONDS.sleep((long) Math.abs((Math.random() * 100)));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if(ATOMIC_STAMPED_REFERENCE.compareAndSet("abc", "abcd", stamp, stamp+1)){
+                if(ATOMIC_REFERENCE.compareAndSet("abc", "abcd")){
                     System.out.println("我是线程：" + num + ",我CAS修改为了abcd!");
                 }
             }).start();
@@ -28,8 +27,7 @@ public class AtomicStampedReferenceDemo1 {
 
         // 轮询CAS变更为原来的值
         new Thread(() -> {
-            int stamp = ATOMIC_STAMPED_REFERENCE.getStamp();
-            while (!ATOMIC_STAMPED_REFERENCE.compareAndSet("abcd", "abc", stamp, stamp+1));
+            while (!ATOMIC_REFERENCE.compareAndSet("abcd", "abc"));
             System.out.println("我是线程：" + Thread.currentThread().getName() + ",我CAS修改回了abc!");
         }).start();
     }
--- class-20-concurrent/src/main/java/com/jsonyao/cs/juc/atomic/aba/MyAtomicStampedReferenceDemo1.java
+++ class-20-concurrent/src/main/java/com/jsonyao/cs/juc/atomic/aba/MyAtomicStampedReferenceDemo1.java
@@ -1,25 +1,26 @@
 package com.jsonyao.cs.juc.atomic.aba;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
- * 测试ABA问题: 同样的abc经过abcd ->abc, 对于第二个线程不应该能够设置成功, 但却设置成功了
+ * 测试AtomicStampedXXX解决ABA问题: 加入版本号, 下一个线程使用上一个版本号会更新失败
  */
-public class AtomicReferenceAbaDemo1 {
+public class MyAtomicStampedReferenceDemo1 {
 
-    public static final AtomicReference<String> ATOMIC_REFERENCE = new AtomicReference<String>("abc");
+    public static final AtomicStampedReference<String> ATOMIC_STAMPED_REFERENCE = new AtomicStampedReference<String>("abc", 0);
 
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++){
             final int num = i;
+            final int stamp = ATOMIC_STAMPED_REFERENCE.getStamp();
             new Thread(() -> {
                 try {
                     TimeUnit.MICROSECONDS.sleep((long) Math.abs((Math.random() * 100)));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if(ATOMIC_REFERENCE.compareAndSet("abc", "abcd")){
+                if(ATOMIC_STAMPED_REFERENCE.compareAndSet("abc", "abcd", stamp, stamp+1)){
                     System.out.println("我是线程：" + num + ",我CAS修改为了abcd!");
                 }
             }).start();
@@ -27,7 +28,8 @@ public class AtomicReferenceAbaDemo1 {
 
         // 轮询CAS变更为原来的值
         new Thread(() -> {
-            while (!ATOMIC_REFERENCE.compareAndSet("abcd", "abc"));
+            int stamp = ATOMIC_STAMPED_REFERENCE.getStamp();
+            while (!ATOMIC_STAMPED_REFERENCE.compareAndSet("abcd", "abc", stamp, stamp+1));
             System.out.println("我是线程：" + Thread.currentThread().getName() + ",我CAS修改回了abc!");
         }).start();
     }
