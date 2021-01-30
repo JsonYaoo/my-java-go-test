bc54ccb6f755b52a66cea1098ddbc0cfe805866f
yaocs2 <yaocs2@midea.com>
2021-01-30 13:19:42 +0800

    my-java-go-test> Rabbit MQ-ConfirmListener机制

---
+++ stage-3-rabbit-mq/src/main/java/com/jsonyao/cs/confirmlistener/BaseClient.java
@@ -0,0 +1,42 @@
+package com.jsonyao.cs.confirmlistener;
+
+/**
+ * Rabbit MQ客户端常量属性
+ */
+public class BaseClient {
+
+    /**
+     * 主机
+     */
+    public static final String HOST = "192.168.1.111";
+
+    /**
+     * 端口
+     */
+    public static final Integer PORT = 5672;
+
+    /**
+     * 虚拟主机
+     */
+    public static final String VIRTUAL_HOST = "/";
+
+    /**
+     * 交换机名称
+     */
+    public static final String EXCHANGE_NAME = "test_confirmlistener_exchange";
+
+    /**
+     * 交换机类型
+     */
+    public static final String EXCHANGE_TYPE = "topic";
+
+    /**
+     * QueueName
+     */
+    public static final String QUEUE_NAME = "test_confirmlistener_queue";
+
+    /**
+     * RoutingKey
+     */
+    public static final String ROUTING_KEY = "confirm.#";
+}
---
+++ stage-3-rabbit-mq/src/main/java/com/jsonyao/cs/confirmlistener/Sender.java
@@ -0,0 +1,49 @@
+package com.jsonyao.cs.confirmlistener;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Producer: 测试Fanout交换机
+ */
+public class Sender extends BaseClient {
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1. 创建ConnectionFactory
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost(HOST);
+        connectionFactory.setPort(PORT);
+        connectionFactory.setVirtualHost(VIRTUAL_HOST);
+
+        // 2. 创建Connection
+        Connection connection = connectionFactory.newConnection();
+
+        // 3. 创建Channel
+        Channel channel = connection.createChannel();
+
+        // topic类型交换机: 实际Routing_key
+        String routingKey1 = "confirm.save";
+
+        // 4. 发送消息: Queue绑定Exchange: 表示交换机EXCHANGE_NAME上ROUTING_KEY的消息会路由到QUEUE_NAME中
+        String msg = "Hello World RabbitMQ 4 FANOUT Exchange Message... ";
+        channel.confirmSelect();// 开启Confirm消息机制
+        channel.addConfirmListener(new ConfirmListener() {
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                System.out.println("------- ok ---------" + deliveryTag);
+            }
+
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                System.err.println("------- error ---------" + deliveryTag);
+            }
+        });
+        channel.basicPublish(EXCHANGE_NAME, routingKey1, null, msg.getBytes());
+        System.out.println("发送消息完毕...");
+    }
+}
